fceca1a7cee3b27dd8151bf4f1bd07766613851c
BreadTeleporter2009 <103989916+BreadTeleporter2009@users.noreply.github.com>
2022-05-12 13:03:47 +0100

    First Commit

---
+++ main.cs
@@ -0,0 +1,44 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    string command;
+    string user = Environment.UserDomainName;
+    Console.WriteLine($"Welcome to Next TERM Alpha 1, {user}!");
+    while (true==true)
+    {
+      string cwd = System.AppContext.BaseDirectory;
+      Console.Write($"{cwd}:$ ");
+      command = Console.ReadLine();
+      Console.WriteLine("\n");
+      if (command.Contains("cd"))
+      {
+        string directory=command.Replace("cd ", "");
+        Environment.CurrentDirectory = directory;
+      }
+      if (command=="whoami")
+      {
+        Console.WriteLine($"You are currently logged in as: {user}");
+      }
+      else if (command=="ver" || command=="version")
+      {
+        Console.WriteLine("Next TERM Alpha 1");
+      }
+      else
+      {
+        try
+        {
+          Console.WriteLine("\n");
+          System.Diagnostics.Process.Start(command);   
+          Console.WriteLine("\n");
+        }
+        catch
+        {
+          Console.WriteLine("An error has occured.");
+        }
+      }
+      
+    }
+
+  }
+}
